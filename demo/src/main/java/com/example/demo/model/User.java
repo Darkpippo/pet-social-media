fd08d5700af40ca8f459b0a0df1f131e1838dfd7
atanasov16 <dakager@gmail.com>
2024-07-03 14:43:58 +0200

    added user model #not fully implemented

---
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,45 @@
+package com.example.demo.model;
+
+import com.example.demo.model.exceptions.InvalidCredentialsException;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
+    private String city;
+
+    public User() {
+
+    }
+
+    public User(String username, String password, String firstName, String lastName, String email, String phone) {
+        if(email==null && phone==null){
+            throw new InvalidCredentialsException();
+        }
+        if(email!=null && email.isEmpty()){
+            throw new InvalidCredentialsException();
+        }
+        if(phone!=null && phone.isEmpty()){
+            throw new InvalidCredentialsException();
+        }
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+    }
+}
---
+++ demo/src/main/java/com/example/demo/model/exceptions/InvalidCredentialsException.java
@@ -0,0 +1,5 @@
+package com.example.demo.model.exceptions;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException() {super("Invalid credentials error");}
+}
